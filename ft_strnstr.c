f3a3244f13b0cbaa4bc04f2a3758da82e04f4b4d
Alejandro Gomez Urrea <agomez-u@car13s4.42barcelona.com>
2023-01-16 20:19:36 +0100

    ft_strnstr.c yet to start

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,34 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: agomez-u <agomez-u@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/16 19:45:53 by agomez-u          #+#    #+#             */
-/*   Updated: 2023/01/16 20:15:32 by agomez-u         ###   ########.fr       */
+/*   Created: 2023/01/16 20:16:15 by agomez-u          #+#    #+#             */
+/*   Updated: 2023/01/16 20:19:12 by agomez-u         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "libft.h"
 
-int
-	memcmp(const void *s1, const void *s2, size_t n);
+char
+	*strnstr(const char *haystack, const char *needle, size_t len);
 
-int
-	memcmp(const void *s1, const void *s2, size_t n)
+char
+	*strnstr(const char *haystack, const char *needle, size_t len)
 {
-	size_t	i;
-	const unsigned char	*str1;
-	const unsigned char	*str2;
 
-	i = 0;
-	str1 = (const unsigned char *)s1;
-	str2 = (const unsigned char *)s2;
-	while ((*(str1 + i) && *(str2 + i)) && i < n)
-	{
-		if (*(str1 + i) != *(str2 + i))
-			return (*(str1 + i) - *(str2 + i));
-		i++;
-	}
-	return (0);
-}
