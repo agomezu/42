65181efac3ac956c1e62565d628e1e346dea3329
Alejandro Gomez Urrea <agomez-u@car13s4.42barcelona.com>
2023-01-16 20:33:31 +0100

    ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: agomez-u <agomez-u@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 20:16:15 by agomez-u          #+#    #+#             */
-/*   Updated: 2023/01/16 20:19:12 by agomez-u         ###   ########.fr       */
+/*   Updated: 2023/01/16 20:32:51 by agomez-u         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,29 @@ char
 char
 	*strnstr(const char *haystack, const char *needle, size_t len)
 {
+	size_t	i;
+	unsigned const char	*s1;
+	unsigned const char	*s2;
 
+	if (!needle)
+		return (haystack);
+	else
+	{
+		i = 0;
+		s1 = (unsigned const char *)haystack;
+		s2 = (unsigned const char *)needle;
+		while (i < len)
+		{
+			if (*(s1 + i) != *(s2 + i))
+				continue ;
+			else
+				break ;
+		i++;
+		}
+		if (*(s2 + i) == 0)
+			return (0);
+		else if ((*(s1 + i) == *(s2 + i)) && i < len)
+			return (*(s2 + i));
+	}
+	return (0);
+}
