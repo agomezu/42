731915d4c38dd779c0794f4d758b6398aa7bc6af
Alejandro Gomez Urrea <agomez-u@cbr8s1.42barcelona.com>
2023-01-17 19:39:05 +0100

    strnstr is not as easy as i thought

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,41 +6,46 @@
 /*   By: agomez-u <agomez-u@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 20:16:15 by agomez-u          #+#    #+#             */
-/*   Updated: 2023/01/16 20:32:51 by agomez-u         ###   ########.fr       */
+/*   Updated: 2023/01/17 19:38:28 by agomez-u         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+// #include "libft.h"
+#include <stdio.h>
 
 char
-	*strnstr(const char *haystack, const char *needle, size_t len);
+	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+
+int	main(void)
+{
+	char	s1[] = "www";
+	char	s2[] = "https://www.hello.www";
+
+	printf("%s\n", ft_strnstr(s2, s1, 30));
+	return (0);
+}
 
 char
-	*strnstr(const char *haystack, const char *needle, size_t len)
+	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	i;
-	unsigned const char	*s1;
-	unsigned const char	*s2;
+	size_t	j;
 
-	if (!needle)
-		return (haystack);
-	else
+	i = 0;
+	j = 0;
+	if (*(needle + i) == 0)
+		return ((char *)haystack);	
+	while (*(haystack + i) && (i < len))
 	{
-		i = 0;
-		s1 = (unsigned const char *)haystack;
-		s2 = (unsigned const char *)needle;
-		while (i < len)
+		if (*(haystack + i) == *(needle + j))
 		{
-			if (*(s1 + i) != *(s2 + i))
-				continue ;
-			else
-				break ;
-		i++;
 		}
-		if (*(s2 + i) == 0)
-			return (0);
-		else if ((*(s1 + i) == *(s2 + i)) && i < len)
-			return (*(s2 + i));
+		i++;
+	}
+	if ((*(s1 + i) == *(s2 + i)) && i < len)
+		return ((char*)(s2 + i));
+	else
+		return (0);
 	}
 	return (0);
 }
