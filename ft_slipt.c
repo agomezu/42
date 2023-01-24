5f146e0130028763be9048f2df076a9c1aa86a42
Alejandro Gomez Urrea <agomez-u@car1s3.42barcelona.com>
2023-01-24 20:48:47 +0100

    libft

--- ft_slipt.c
+++ ft_slipt.c
@@ -6,7 +6,7 @@
 /*   By: agomez-u <agomez-u@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/21 10:05:28 by agomez-u          #+#    #+#             */
-/*   Updated: 2023/01/21 23:19:07 by agomez-u         ###   ########.fr       */
+/*   Updated: 2023/01/24 19:56:45 by agomez-u         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,14 @@ char
 char
 	**ft_split(char const *s, char c)
 {
+	size_t	i;
+	int	count;
+	char	**str;
+	i = 0;
+	count = 0;	
+	while (*(s + i))
+		if (*(s + i) == c)
+		       count++;
+	str = (char**)malloc(sizeof(*s) * count + 1);		
 	
-	char	*str;
-
+}
