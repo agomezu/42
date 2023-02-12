b06add9663da11525304f374984a4112eb13e4a2
agomez-u <agomez-u@student.42barcelona.com>
2023-02-12 21:27:18 +0100

    strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: agomez-u <agomez-u@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 08:29:25 by agomez-u          #+#    #+#             */
-/*   Updated: 2023/02/12 21:25:03 by agomez-u         ###   ########.fr       */
+/*   Updated: 2023/02/12 21:27:11 by agomez-u         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,9 +30,9 @@ char
 		s++;
 	}
 	if (c == '\0')
-		return ((char*)s);
+		return ((char*)++s);
 	if (ret)
 		return ((char*)ret);
-	if (!ret)	
+	else	
 		return ((char*)NULL);
 }
