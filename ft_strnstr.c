1bf0211ad18e529b1ce764fc1908b09bdd8aa22f
agomez-u <agomez-u@student.42barcelona.com>
2023-02-12 22:21:30 +0100

    strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: agomez-u <agomez-u@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 20:16:15 by agomez-u          #+#    #+#             */
-/*   Updated: 2023/02/12 22:07:31 by agomez-u         ###   ########.fr       */
+/*   Updated: 2023/02/12 22:21:13 by agomez-u         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,19 @@ char
 	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {	
 	size_t	i;
+	size_t	j;
 
-	if (!needle)
+	if (*(needle + 0) == '\0')
 		return ((char*)haystack);	
 	i = 0;
-	while (*(haystack) && i < len)
+	while (*(haystack + i) && i < len)
 	{
-		if (*(haystack) == *(needle))
-			return ((char*)haystack);
-		++haystack;
+		j = 0;
+		while (*(haystack + i) != *(needle + j) && *(needle + j))
+			j++;
+		if (*(haystack + i) == *(needle + j))
+			return ((char*)(haystack + i));
+		i++;
 	}
 	return ((char*)NULL);
 }
-
