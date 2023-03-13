e61426361d618ef1dd57e704ecc641bc283069b6
agomezu <119058152+agomezu@users.noreply.github.com>
2023-03-13 17:48:58 +0000

    libft

--- ft_lstnew_bonus.c
+++ ft_lstnew_bonus.c
@@ -24,3 +24,10 @@ t_list
 	x->next = NULL;
 	return (x);
 }
+
+// DESCRIPTION
+/* Allocates (with malloc(3)) and returns a new node.
+/* The member variable ’content’ is initialized with
+/* the value of the parameter ’content’. The variable
+/* ’next’ is initialized to NULL.
+/*
