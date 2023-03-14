4be73411c112ddefe74e7a2f5706a1d4e1e04559
Ale Gómez Urrea <agomezu@iMac-de-Ale.local>
2023-03-14 19:49:22 +0100

    libft

--- ft_lstsize_bonus.c
+++ ft_lstsize_bonus.c
@@ -6,7 +6,7 @@
 /*   By: agomez-u <agomez-u@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/29 18:07:47 by agomez-u          #+#    #+#             */
-/*   Updated: 2023/02/23 19:52:25 by agomez-u         ###   ########.fr       */
+/*   Updated: 2023/03/14 19:49:11 by agomez-u         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,3 +27,31 @@ int
 	}
 	return (i);
 }
+
+// TEST
+/*int	main(void)
+{
+	t_list	*head = NULL;
+	t_list	*second = NULL;
+	t_list	*third = NULL;
+	int	n;
+
+	// Allocate 3 nodes in the linked list
+	// Assign values to each node
+	head = ft_lstnew("This is the first node");
+	second = ft_lstnew("This is the second node");
+	third = ft_lstnew("This is the third node");
+	head->next = second;
+	second->next = third;
+	n = (int)ft_lstsize(head);
+	printf("Number of nodes in the list: %d\n", n);
+	free(head);
+	free(second);
+	free(third);
+	return (0);
+}
+*/
+
+// DESCRIPTION
+/* Counts the number of nodes in a list.
+ */
