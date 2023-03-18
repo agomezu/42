b237c25f5650f99cc21c26fb023777c9c07ffb28
Ale Gómez Urrea <agomezu@iMac-de-Ale.local>
2023-03-18 08:10:33 +0100

    libft

---
+++ ft_lstiter_bonus.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstiter_bonus.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agomez-u <agomez-u@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/30 19:01:41 by agomez-u          #+#    #+#             */
+/*   Updated: 2023/02/23 19:45:35 by agomez-u         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void
+	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	t_list	*ptr;
+
+	if (lst)
+	{
+		ptr = lst;
+		while (1)
+		{
+			(*f)(ptr->content);
+			ptr = ptr->next;
+			if (ptr == NULL)
+				return ;
+		}
+	}
+}
---
+++ ft_lstmap_bonus.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agomez-u <agomez-u@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/07 18:59:32 by agomez-u          #+#    #+#             */
+/*   Updated: 2023/02/23 19:45:58 by agomez-u         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list
+	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new;
+	t_list	*list;
+
+	if (!lst || !f || !del)
+		return (0);
+	new = ft_lstnew((f)(lst->content));
+	if (!new)
+		return (0);
+	list = new;
+	lst = lst->next;
+	while (lst)
+	{
+		new->next = ft_lstnew((f)(lst->content));
+		if (!new->next)
+		{
+			ft_lstclear(&list, del);
+			return (0);
+		}
+		new = new->next;
+		lst = lst->next;
+	}
+	new->next = NULL;
+	return (list);
+}
